537a93d9daf651d5ea614ed3e838f2c129813b2e
Made882 <madeline.munoz.barre@utelvt.edu.ec>
2021-10-19 16:07:30 -0500

    Trabajo Madeline

---
+++ madesuma.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float x,y,z;
+cout<<"Ingrese 2 numeros separados del espacio x y y:";
+cin>>x>>y;
+z=x+y;
+cout<<"El resultado es: ";
+cout<<z<<endl;
+return 0;
+}	
