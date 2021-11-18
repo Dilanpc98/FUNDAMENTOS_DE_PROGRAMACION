4bbf7bdb818343502b15bedbb812d723ab7876a8
Dilanpc98 <dilan.garcia.coime@utelvt.edu.ec>
2021-11-18 18:23:33 -0500

    Actualizacion de carpetas

---
+++ Trabajos_1Parcial/angiesuma.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	float x,y,z;
+	cout<<"Ingrese 2 numeros separados de espacio x y y :";
+	cin>>x>>y;
+        z=x+y;
+	cout<<"El Resultado de la suma es :";
+	cout<<z<<endl;
+	return 0;
+}
