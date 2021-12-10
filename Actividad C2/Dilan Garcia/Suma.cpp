fbe69f7ca884e5c40d2e57dc98870337be63c50b
Dilanpc98 <dilan.garcia.coime@utelvt.edu.ec>
2021-12-10 08:10:35 -0500

    Actividad C2

---
+++ Actividad C2/Dilan Garcia/Suma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	float a,b,c;
+	cout<<"Ingrese el primer numero: "; cin>>a;
+	cout<<"Ingrese el segundo  numero: "; cin>>b;
+	c=a+b;
+	cout<<"El resultado de la suma de "<<a<<" + "<<b<<" = "<<c<<endl;
+	return 0;
+}
