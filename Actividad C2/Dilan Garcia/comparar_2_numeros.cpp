2e69d6885de76b277924dd3467f40678be66da12
Dilanpc98 <dilan.garcia.coime@utelvt.edu.ec>
2021-12-16 18:16:11 -0500

    Actividad C2

---
+++ Actividad C2/Dilan Garcia/comparar_2_numeros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+float x,y;
+cout<<"Ingrese x : "; cin>>x;
+cout<<"Ingrese y : "; cin>>y;
+if(x==y)
+{
+	cout<<"El valor de x = "<<x<<" es igual a y = "<<y<<endl;
+}else {
+	if (x<y){
+		cout<<"El valor de x = "<<x<<" Es menor a y = "<<y<<endl;
+	}else{
+		cout<<"El valor de y = "<<y<<" Es menor a x = "<<x<<endl;
+	}
+}
+}
