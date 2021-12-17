c8e880687e5be8e5d640e439fecb911481c27982
Made882 <madeleinmunoz0217@gmail.com>
2021-12-16 20:12:36 -0500

    Actividad C2

---
+++ Actividad C2/Muñoz Madeline/sumasimple.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float x,y,z;
+cout<< "Ingrese el numero x ";
+cin>> x;
+cout<< "Ingrese el numero y ";
+cin>> y;
+z=x+y;
+cout<< "El resultado de la suma es: "<<z<<endl;
+return 0;
+}	
