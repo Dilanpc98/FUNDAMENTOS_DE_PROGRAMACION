935d0f4096e99c0fda7fae7ea40d6cf8c639782c
cando6 <angie.cando.estupinan@utelvt.edu.ec>
2021-12-18 20:43:04 -0500

    Actividad C2

---
+++ Actividad C2/Cando Angie/comparacion_numeros.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+float x,y;
+cout<<"Ingrese x: "; cin>>x;
+cout<<"Ingrese y: "; cin>>y;
+if(x==y)
+{
+	cout<<"El valor de x = "<<x<<" es igual a y = "<<y<<endl;
+}else{
+	if(x<y){
+	cout<<"El valor de x = "<<x<<" es menor a y = "<<y<<endl;
+	}else{	
+	cout<<"El valor de y = "<<y<<" es menor a x = "<<x<<endl;
+	}
+}
+return 0;
+}
+
---
+++ Actividad C2/Cando Angie/suma_numeros.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	float x,y,z;
+	cout<<"Ingrese el primer numero: "; cin>>x;
+	cout<<"Ingrese el segundo numero: "; cin>>y;
+z=x+y;
+cout<<"El resultado de la suma es: "<<x<<" + "<<y<<" = "<<z<<endl;
+return 0;
+}
